ca531abdfe1410d40bae0e6768dcf2b4cad42166
Justin13wyx <justin13wyx@gmail.com>
2017-12-03 15:23:32 +0800

    Fix bugs and add new function of reverse

--- ascii.py
+++ ascii.py
@@ -5,11 +5,37 @@
 # mail:justin13wyx@gmail.com
 # Created Time: Sun Dec  3 14:50:30 2017
 # ==============================================================
-import sys
+import sys, getopt
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        char = sys.argv[1]
-        res = ord(char)
-        print(res)
-        
+    reverse = 0
+    try:
+        opts, argv = getopt.getopt(sys.argv[1:], "r", ["reverse"])
+    except getopt.GetoptError:
+        print("Bad param.")
+        exit(-1)
+
+    for val in opts:
+        val = val[0]
+        if val in ["-r", "--reverse"]:
+            reverse = 1
+
+    try:
+        ques = argv[0]
+        try:
+            int(ques)
+        except ValueError:
+            if reverse == 0:
+                print(ord(ques))
+                exit(0)
+            else:
+                print("I guess you don't need -r|--reverse flag.")
+                exit(-3)
+        if int(ques) > 9 and reverse == 0:
+             print("I guess this is code but you don't give the -r|--reverse flag.\nMay check it again?")
+             exit(-3)
+    except IndexError:
+        print("Miss question.")
+        exit(-2)
+
+    print("{}".format(chr(int(ques)) if reverse else ord(ques)))
